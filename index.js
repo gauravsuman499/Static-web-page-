beb079e70abefc8980bac85dbdbe1587c24ee61c
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2023-01-11 19:12:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const nav = 
+<nav>
+    <h1>MY FIRST REACT STATIC PAGE</h1>
+    <h1>Gold Fast Food</h1>
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Pricing</li>
+        <li>Contact</li>
+    </ul>
+</nav>
+ReactDOM.render(nav , document.getElementById("root"))
